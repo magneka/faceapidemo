4c8fe5e27406825b6b9bb037cb5b51da9f570992
Magne Alvheim <magne@alvheim.com>
2022-02-10 17:57:26 +0100

    fire

--- src/pages/FaceMatcher/FaceMatcherContainer.js
+++ src/pages/FaceMatcher/FaceMatcherContainer.js
@@ -7,18 +7,35 @@ export const FaceMatcherContainer = (props) => {
     const canvasRef = useRef(null);
 
     const faceMatcher = props.faceMatcher
-
-    // Viser bilde på skjermen
-    let image = await faceapi.bufferToImage(event.target.files[0])
-    imageRef.current.src = image.src
-
+    
     const imageOnChange = async (event) => {
-        
+
         console.log('We are loading a new image')
       
         // Viser bilde på skjermen
         let image = await faceapi.bufferToImage(event.target.files[0])
         imageRef.current.src = image.src
+
+        // Lager et canvase basert på bildet
+        let canvas = canvasRef.current
+        canvasRef.current = faceapi.createCanvasFromMedia(image)
+
+        // Sett dimensjonenen på bildet og canvas likt
+        const displaySize = { width: image.width, height: image.height }
+        faceapi.matchDimensions(canvas, displaySize) 
+        canvasRef.current = canvas
+
+        // Kjør ansiktsdeteksjon
+        const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+        const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
+
+        // Iterer gjennom resutatene
+        results.forEach((result, i) => {
+            const box = resizedDetections[i].detection.box
+            const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
+            drawBox.draw(canvas)
+        }) 
     }
 
     return (
